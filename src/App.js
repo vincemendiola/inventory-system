c7017d7a406fd9c8f99bb4386bef49dd8ab60fe4
Albert Vince Mendiola <albert@mediajel.com>
2024-09-03 02:24:49 +0800

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
+
+import { Layout, Menu } from "antd";
+
+const { Header, Sider, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider width={200} className="site-layout-background">
+        <div style={{ padding: "16px", textAlign: "center" }}>
+          <img src={logo} style={{ width: "100px", height: "auto" }} />
+          <div style={{ color: "#fff", marginTop: "8px", fontSize: "16px" }}>
+            Your App Name
+          </div>
+        </div>
+        <Menu
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          style={{ height: "100%", borderRight: 0 }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Menu.Item key="1">Inventory</Menu.Item>
+          <Menu.Item key="2">Users</Menu.Item>
+          <Menu.Item key="3">Item Types</Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout>
+        <Header
+          className="site-layout-sub-header-background"
+          style={{ padding: 0 }}
+        >
+          <p style={{ color: "white" }}>Inventory System</p>
+        </Header>
+        <Content style={{ padding: "0 24px", minHeight: 280 }}>
+          <div>Sample Dummy Body Content</div>
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
 
